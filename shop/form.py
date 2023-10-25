011919eef1fc7be6f1b535bc8ae4b128cc5aee7d
Dhananjay692 <83397379+Dhananjay692@users.noreply.github.com>
2023-10-25 20:00:11 +0530

    Add files via upload

---
+++ shop/form.py
@@ -0,0 +1,14 @@
+from pyexpat import model
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+ 
+class CustomUserForm(UserCreationForm):
+  username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter User Name'}))
+  email=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Email Address'}))
+  phone=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Phone Number'}))
+  password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Your Password'}))
+  password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Confirm Password'}))
+  class Meta:
+    model=User
+    fields=['username','email','phone','password1','password2']
